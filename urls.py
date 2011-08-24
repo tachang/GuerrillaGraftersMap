f6cb686e5cc22af43f7069ea84bb6add1fa2c9fb
Jesse Bounds <jesse@rebounds.net>
2011-08-24 09:04:05 -0700

    merge of index and about mockups into explore (root) and about templates, adding back (probably) missing jpgs, adding about page view controller

--- urls.py
+++ urls.py
@@ -8,7 +8,11 @@ urlpatterns = patterns('',
 
     (r'^admin/', include(admin.site.urls)),
     (r'^explore/$', 'sanfran.views.explore'),
+    (r'^about/$', 'sanfran.views.about'),
     (r'^$', 'sanfran.views.explore'),
+	# temporary: to view static files on jesse's dev machine
+	# (r'^public/(?P<path>.*)$', 'django.views.static.serve',
+	#        {'document_root': '/Users/jesse/Documents/github/ggrafters_tachang/GuerrillaGraftersMap/public'}),	
     # Example:
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),	
