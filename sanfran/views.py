0b18b0d8214e1e2b7ff4f1e66be35ee5017bf427
Jeff Tchang <Jeff.Tchang@gmail.com>
2011-08-28 21:51:56 -0700

    Temporarily removed the neighborhood shape files.

--- sanfran/views.py
+++ sanfran/views.py
@@ -6,10 +6,11 @@ from sanfran.models import PlanningNeighborhoods
 log = logging.getLogger(__name__)
 
 def explore(request):
-    neighborhoods = PlanningNeighborhoods.objects.all()
+    #neighborhoods = PlanningNeighborhoods.objects.all()
+    neighborhoods = []
 
-    for neighborhood in neighborhoods:
-        neighborhood.geom.transform(900913)
+    #for neighborhood in neighborhoods:
+    #    neighborhood.geom.transform(900913)
 
     t = loader.get_template('sanfran/explore.html')
     c = Context({
